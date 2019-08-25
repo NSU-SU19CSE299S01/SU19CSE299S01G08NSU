b48b2ec4a4045ef8273e9c3883fd6c83a46d9f24
sagurhossan <ashraful.sagur@northsouth.edu>
2019-08-25 12:26:29 +0600

    Create viewAllCouriers.php

---
+++ viewAllCouriers.php
@@ -0,0 +1,157 @@
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+ 
+        
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="images/homepage/favicon.ico" type="image/x-icon">
+<title>Online Percel Service</title>
+	<meta name="author"	content="Audenberg Technologies (www.audenberg.com)" />
+<link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+ <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link href="css/animate.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
+
+    </head>
+    <body>
+     
+          <div class="container-fluid" style=" background-image: url(images/deli.jpg); background-repeat: no-repeat; background-size: cover; height: 100vh;">
+         <style>
+        <style>
+
+h1 {
+  font-size:3em; 
+  font-weight: 300;
+  line-height:1em;
+  text-align: center;
+  color: #4DC3FA;
+}
+
+h2 {
+  font-size:1em; 
+  font-weight: 300;
+  text-align: center;
+  display: block;
+  line-height:1em;
+  padding-bottom: 2em;
+  color: #FB667A;
+}
+
+h2 a {
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #FB667A;
+  text-decoration: none;
+}
+
+.blue { color: #185875; }
+.yellow { color: #FFF842; }
+
+.container th h1 {
+	  font-weight: bold;
+	  font-size: 1em;
+  text-align: left;
+  color: #fff;
+}
+
+.container td { color: #fff;
+	  font-weight: normal;
+	  font-size: 1em;
+  -webkit-box-shadow: 0 2px 2px -2px #0E1119;
+	   -moz-box-shadow: 0 2px 2px -2px #0E1119;
+	        box-shadow: 0 2px 2px -2px #0E1119;
+}
+
+.container {
+	  text-align: left;
+	  overflow: hidden;
+	  width: 80%;
+	  margin: 0 auto;
+  display: table;
+  padding: 0 0 8em 0;
+  box-shadow: 0px 0px 25px #0E1119;
+}
+
+.container td, .container th {
+	  padding-bottom: 2%;
+	  padding-top: 2%;
+  padding-left:2%;  
+}
+
+/* Background-color of the odd rows */
+.container tr:nth-child(odd) {
+	  background-color: #323C50;
+}
+
+/* Background-color of the even rows */
+.container tr:nth-child(even) {
+	  background-color: #2C3446;
+}
+
+.container th {
+	  background-color: #1F2739;
+}
+
+.container td:first-child { color: #FB667A; }
+
+.container tr:hover {
+   background-color: #464A52;
+-webkit-box-shadow: 0 6px 6px -6px #0E1119;
+	   -moz-box-shadow: 0 6px 6px -6px #0E1119;
+	        box-shadow: 0 6px 6px -6px #0E1119;
+}
+
+.container td:hover {
+  background-color: #FFF842;
+  color: #403E10;
+  font-weight: bold;
+  
+  box-shadow: #7F7C21 -1px 1px, #7F7C21 -2px 2px, #7F7C21 -3px 3px, #7F7C21 -4px 4px, #7F7C21 -5px 5px, #7F7C21 -6px 6px;
+  transform: translate3d(6px, -6px, 0);
+  
+  transition-delay: 0s;
+	  transition-duration: 0.4s;
+	  transition-property: all;
+  transition-timing-function: line;
+}
+
+@media (max-width: 800px) {
+.container td:nth-child(4),
+.container th:nth-child(4) { display: none; }
+}</style>
+        
+        <div class="" style="margin-top:5%; width: 100%;">
+        <h1> ALL COURIERS</h1>
+<table class="container" >
+	<thead>
+		<tr>
+			<th><h1>ID</h1></th>
+			<th><h1>Sent TO</h1></th>
+			<th><h1>Email</h1></th>
+			<th><h1>Mobile</h1></th>
+                        <th><h1>To Address</h1></th>
+			<th><h1>Courier Weight</h1></th>
+			<th><h1>Courier Amount</h1></th>
+			<th><h1>Courier Type</h1></th>
+                        <th><h1>Delivered</h1></th>
+		</tr>
+	</thead>
+	<tbody>
+               
+	</tbody>
+</table>
+        </div>
+        
+        
+    </body>
+   
+</html>
