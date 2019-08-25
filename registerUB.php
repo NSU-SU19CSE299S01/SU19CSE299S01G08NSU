20586d13519f25505267fa885ece3aa2e7d4a986
sagurhossan <ashraful.sagur@northsouth.edu>
2019-08-25 12:10:10 +0600

    Update registerUB.php

--- registerUB.php
+++ registerUB.php
@@ -39,6 +39,44 @@
 </html>
 
 
+<html>
+    <head>
+        <link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body style="background: url(img/bgbg.png);height: 100vh;">
+        <div style="">
+        
+        </div>
+        
+        <!-- Trigger the modal with a button -->
+<button id="modalBtn" type="button" style="display:none" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        
+      <h3 >Your Branch has been registered.. Login to continue using our services</h3>
+     <br>
+     <a href="index.php?msg=login">Login</a>
+    </div>
+    </div>
+</div>
+    
+    <script>
+     $('#modalBtn').trigger("click");
+    </script>
+    </body>
+</html>
 
 <?php
     $conn->close();
