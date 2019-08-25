aa82d8a2470ca0b1e775ccbe4445915c66cf5e43
sagurhossan <ashraful.sagur@northsouth.edu>
2019-08-25 11:54:30 +0600

    Create UserPage.php

---
+++ UserPage.php
@@ -0,0 +1,78 @@
+
+<?php
+session_start();
+if(!isset($_SESSION["uid"])){
+    header('Location:index.php');
+}
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+ 
+        
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="images/homepage/favicon.ico" type="image/x-icon">
+<title>Online Percel Service</title>
+	<meta name="author"	content="Audenberg Technologies (www.audenberg.com)" />
+<link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+ <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link href="css/animate.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
+<style>h5 {
+  display: inline-block;
+  padding: 10px;
+  background: #B9121B;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.full-screen {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}</style>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        include 'navBar.php';
+         ?>
+        <div class="container-fluid" style=" background-image: url(images/bg11.jpg); background-repeat: no-repeat; background-size: cover; height: 100vh;">
+            
+            <div class="row" style="width: 100%; margin-top: 10%">
+        
+                <div class="col-md-6"><a href="addCourier.php" >
+                    <div class="container" style="width: 300px; height: 200px;color: #fff;
+    background-color:  #83c11f; text-align: center;
+    border-color: #83c11f; box-shadow: 0px 0px 10px #000;
+    
+    ">
+                        <span class="fa fa-rocket" style="  font-size: 50px; padding: 25px;"></span>
+                         <h2>Add new Couriers</h2>
+                    </div></a>
+                    <a href="viewCouriers.php" >
+                    <div class="container" style="width: 300px; height: 200px;color: #fff;
+    background-color:  #83c11f; text-align: center;
+    border-color: #83c11f; box-shadow: 0px 0px 10px #000;
+    
+    ">
+                        <span class="fa fa-book" style="  font-size: 50px; padding: 25px;"></span>
+                         <h2>View My Couriers</h2>
+                    </div></a>
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        
+    </body>
+   
+</html>
