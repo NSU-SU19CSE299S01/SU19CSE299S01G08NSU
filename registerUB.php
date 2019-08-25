d7ee5b2c562f94e4aa63e9d0602578c3b20ac2db
sagurhossan <ashraful.sagur@northsouth.edu>
2019-08-25 12:09:46 +0600

    Create registerUB.php

---
+++ registerUB.php
@@ -0,0 +1,60 @@
+
+<html>
+    <head>
+        <link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/Animate.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+ <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body style="background: url(img/bgbg.png);height: 100vh;">
+        <div style="">
+        
+        </div>
+        
+        <!-- Trigger the modal with a button -->
+<button id="modalBtn" type="button" style="display:none" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        
+      <h3 >User has been registered.. Login to continue using our services</h3>
+     <br>
+     <a href="index.php">Login</a>
+    </div>
+    </div>
+</div>
+    
+    <script>
+     $('#modalBtn').trigger("click");
+    </script>
+    </body>
+</html>
+
+
+
+<?php
+    $conn->close();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+        
+        
+    }
+    
+
+     
+    
+//$conn->close();
+}else{
+    header("location : index.php");
+}
+
