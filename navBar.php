b2d352fe1495b9ab0924dad6387c22d589cd3506
sagurhossan <ashraful.sagur@northsouth.edu>
2019-08-25 12:07:04 +0600

    Update navBar.php

--- navBar.php
+++ navBar.php
@@ -1,5 +1,16 @@
 
-<
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" style="box-shadow: 0px 3px 4px rgba(225, 225, 225, .6); font-family: 'Kodchasan', sans-serif;">
+  <div class="container-fluid"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#" style="">Online percel Service          </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="defaultNavbar1">
+     
+   
+    <ul class="nav navbar-nav navbar-right ">
+        <li><a href="index.php">HOME</a></li>
         	<?php 
                 
                if (session_status() == PHP_SESSION_NONE) {
